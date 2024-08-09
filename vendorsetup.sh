2e1f7142241dd29d92c4cb65cfb9a68daefd86e5
mikeNG <1216752+mikeNG@users.noreply.github.com>
2024-08-09 07:03:53 +0000

    ivan:track repos from lineage-21

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,13 +19,13 @@ KT=kernel/oneplus/ivan/Makefile
 if ! [ -a $KT ]; then git clone https://github.com/Notganesh/kernel_oneplus_ivan.git -b main kernel/oneplus/ivan
 fi
 MTK_SEPOLICY=device/mediatek/sepolicy_vndr/SEPolicy.mk
-if ! [ -a $MTK_SEPOLICY ]; then git clone --depth=1 https://github.com/Project-Elixir/device_mediatek_sepolicy_vndr -b Tiramisu device/mediatek/sepolicy_vndr
+if ! [ -a $MTK_SEPOLICY ]; then git clone --depth=1 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
 fi
 COMPACT=hardware/lineage/compat/Android.bp
-if ! [ -a $COMPACT ]; then git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_compat -b lineage-20.0 hardware/lineage/compat
+if ! [ -a $COMPACT ]; then git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_compat -b lineage-21.0 hardware/lineage/compat
 fi
 MTK=hardware/mediatek/Android.bp
-if ! [ -a $MTK ]; then git clone --depth=1 https://github.com/Notganesh/android_hardware_mediatek.git -b lineage-20 hardware/mediatek
+if ! [ -a $MTK ]; then git clone --depth=1 https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
 fi
 CLANG17=prebuilts/clang/host/linux-x86/clang-r383902/bin/clang
 if ! [ -a $CLANG17 ]; then git clone --depth=1 https://github.com/oneplus-mediatek-dev/android_prebuilts_clang_host_linux-x86_clang-6443078.git -b 11.0.1 prebuilts/clang/host/linux-x86/clang-r383902
@@ -33,7 +33,7 @@ fi
 WLAN=hardware/mediatek/wlan/Android.mk
 if ! [ -a $WLAN ]; then git clone --depth=1 https://github.com/nishant6342/android_hardware_mediatek_wlan hardware/mediatek/wlan
 fi
-OPLUS=hardware/oplus/Android.bp
-if ! [ -a $OPLUS ]; then git clone --depth=1 git clone https://github.com/crdroidandroid/android_hardware_oplus.git -b 13.0 hardware/oplus
+OPLUS=hardware/oplus/Android.mk
+if ! [ -a $OPLUS ]; then git clone --depth=1 https://github.com/LineageOS/android_hardware_oplus.git -b lineage-21 hardware/oplus
 fi
 echo end cloning
